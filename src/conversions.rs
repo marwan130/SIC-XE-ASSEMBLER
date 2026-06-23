345d3e830111eca576c0061b2433cf14cb61adad
marwan130 <marwanhossam130@gmail.com>
2026-06-23 23:28:18 +0300

    removed unused code from pass1 and got rid of compiler warnings

--- src/conversions.rs
+++ src/conversions.rs
@@ -1,35 +1,3 @@
-use std::collections::HashMap;
-use std::fs::File;
-use std::io::{self, Write};
-
-pub fn string_to_usize(s: &str) -> usize {
-    if s.is_empty() {
-        0
-    } else {
-        s.parse::<usize>().unwrap_or(0)
-    }
-}
-
-pub fn usize_to_string(number: usize) -> String {
-    if number == 0 {
-        String::new()
-    } else {
-        format!("{:04X}", number)
-    }
-}
-
-pub fn hex_to_string(hex: &str) -> String {
-    format!("{:02X}", usize::from_str_radix(hex, 16).unwrap_or(0))  // convert hex string to formatted string
-}
-
-pub fn remove_plus_prefix(instr: &str) -> String {
-    if instr.starts_with('+') {
-        instr[1..].to_string()  // remove + prefix for format 4 instructions
-    } else {
-        instr.to_string()
-    }
-}
-
 pub fn get_register_value(reg: &str) -> u8 {
     match reg.to_uppercase().as_str() {
         "A" => 0,
@@ -43,18 +11,6 @@ pub fn get_register_value(reg: &str) -> u8 {
         "SW" => 9,
         _ => 0,
     }
-}
-
-pub fn set_flag(value: u8, flag: u8, set: bool) -> u8 {
-    if set {
-        value | flag
-    } else {
-        value & !flag
-    }
-}
-
-pub fn get_flag(value: u8, flag: u8) -> bool {
-    (value & flag) != 0
 }
 
 pub fn string_to_hex(s: &str) -> String {
@@ -78,62 +34,3 @@ pub fn integer_to_hex(value: usize, bytes: usize) -> String {
         _ => format!("{:X}", value),
     }
 }
-
-pub trait LocctrExtensions {
-    fn turn_to_hexa(&mut self, i: usize, new_locctr: usize, instr_type: &str, ref_type: &str);
-
-    fn write_to_file(&self, pass1_file: &mut File, i: usize, instr_type: &str, ref_type: &str, labels: &str) -> Result<(), io::Error>;
-
-    fn generate_symbol_table(&self, symbol_table: &mut File, labels_type: &str, i: usize) -> Result<(), io::Error>;
-
-    fn generate_literal_table(&self, literal_table: &mut File, labels_type: &str, i: usize) -> Result<(), io::Error>;
-}
-
-impl LocctrExtensions for HashMap<usize, String> {
-    fn turn_to_hexa(&mut self, i: usize, new_locctr: usize, instr_type: &str, ref_type: &str) {
-        // convert new_locctr to a hexadecimal string
-        let hexalocctr = format!("{:04X}", new_locctr);
-
-        // handle empty strings
-        if instr_type == "BASE" || instr_type == "LTORG" || instr_type == "END"  {
-            if let Some(next_locctr) = self.get(&i).cloned() {
-                self.insert(i, " ".to_string());
-                self.insert(i + 1, next_locctr);
-            }
-        }
-        // if the instruction is equ and ref_type is not a literal, keep the same locctr
-        else if instr_type.starts_with("EQU") && !ref_type.starts_with('*') {
-            if let Some(next_locctr) = self.get(&i).cloned() {
-                self.insert(i, hexalocctr.clone());
-                self.insert(i + 1, next_locctr);
-            }
-        }
-        // default case: update the next locctr
-        else {
-            self.insert(i + 1, hexalocctr.clone());
-        }
-    }
-
-    fn write_to_file(&self, pass1_file: &mut File, i: usize, instr_type: &str, ref_type: &str, labels: &str) -> Result<(), io::Error> {
-        let final_locctr_value = self.get(&i);
-        writeln!(pass1_file, "{:<6} {:<10} {:<8} {}", final_locctr_value.unwrap_or(&String::new()), labels, instr_type, ref_type)?;
-        Ok(())
-    }
-
-    fn generate_symbol_table(&self, symbol_table: &mut File, labels_type: &str, i: usize) -> Result<(), io::Error> {
-        if labels_type != "&" && labels_type != "*" {
-            let final_locctr_value = self.get(&i);
-            writeln!(symbol_table, "{:<10} {}", labels_type, final_locctr_value.unwrap_or(&String::new()))?;
-        }
-        Ok(())
-    }
-
-    fn generate_literal_table(&self, literals_table: &mut File, literals_type: &str, i: usize) -> Result<(), io::Error> {
-        if literals_type.starts_with('=') {
-            let final_locctr_value = self.get(&i);
-            writeln!(literals_table, "{:<10} {}", literals_type, final_locctr_value.unwrap_or(&String::new()))?;
-        }
-        Ok(())
-    }
-}
-
--- src/pass1.rs
+++ src/pass1.rs
@@ -1,19 +1,11 @@
-use crate::conversions::*;
 use std::fs::File;
 use std::io::{self, Write, BufRead};
 use std::collections::HashMap;
-use itertools::izip;
 pub struct Pass1 {
     pub lines: Vec<String>,
     pub labels: Vec<String>,
     pub instr: Vec<String>,
     pub ref_data: Vec<String>,
-    pub literals: HashMap<usize, String>,
-    pub locctr: HashMap<usize, String>,
-    pub default_locctr: HashMap<usize, String>,
-    pub defaultb_locctr: HashMap<usize, String>,
-    pub cdata_locctr: HashMap<usize, String>,
-    pub cblks_locctr: HashMap<usize, String>,
 }
 
 impl Pass1 {
@@ -23,12 +15,6 @@ impl Pass1 {
             labels: Vec::new(),
             instr: Vec::new(),
             ref_data: Vec::new(),
-            literals: HashMap::new(),
-            locctr: HashMap::new(),
-            default_locctr: HashMap::new(),
-            defaultb_locctr: HashMap::new(),
-            cdata_locctr: HashMap::new(),
-            cblks_locctr: HashMap::new(),
         }
     }
 
@@ -73,17 +59,6 @@ impl Pass1 {
                 _ => {}
             }
 
-        }
-
-        for i in 0..self.ref_data.len() {
-            if self.ref_data[i].starts_with("=") {
-                self.literals.insert(i, self.ref_data[i].clone());
-            }
-        }
-
-        let mut intermediate_file = File::create("outputs/intermediate.txt")?;
-        for (label, instr, ref_data) in izip!(&self.labels, &self.instr, &self.ref_data) {
-            writeln!(intermediate_file, "{:<10} {:<8} {}", label, instr, ref_data)?;
         }
 
         Ok(())
@@ -267,202 +242,6 @@ impl Pass1 {
         }
     }
 
-    fn get_symbol_block(&self, _label: &str, _block_locctrs: &HashMap<String, usize>) -> String {
-        "DEFAULTB".to_string()
-    }
-
-    fn get_literal_block(&self, _literal: &str, _block_locctrs: &HashMap<String, usize>) -> String {
-        "DEFAULTB".to_string()
-    }  
-
-    fn initialize_location_counters(&mut self) {
-        self.locctr.insert(1, format!("{:04X}", usize::from_str_radix(&self.ref_data[0], 16).unwrap()).to_string());
-        self.default_locctr.insert(1, format!("{:04X}", usize::from_str_radix(&self.ref_data[0], 16).unwrap()).to_string());
-        self.defaultb_locctr.insert(1, format!("{:04X}", usize::from_str_radix(&self.ref_data[0], 16).unwrap()).to_string());
-        self.cdata_locctr.insert(1, format!("{:04X}", usize::from_str_radix(&self.ref_data[0], 16).unwrap()).to_string());
-        self.cblks_locctr.insert(1, format!("{:04X}", usize::from_str_radix(&self.ref_data[0], 16).unwrap()).to_string());
-    }    
-
-    fn calculate_difference(&self, input: &str) -> usize {
-        if input.contains('-') {
-            let parts: Vec<&str> = input.split('-').collect();
-            if parts.len() == 2 {
-                let (part1, part2) = (parts[0].trim(), parts[1].trim());
-    
-                let mut value1 = 0;
-                let mut value2 = 0;
-    
-                // check if the first part is a label and exists in `labels`
-                if let Some(index1) = self.labels.iter().position(|label| label == part1) {
-                    if let Some(locctr_value1) = self.locctr.get(&index1) {
-                        if let Ok(locctr_numeric1) = usize::from_str_radix(locctr_value1, 16) {
-                            value1 = locctr_numeric1;
-                        }
-                    }
-                } else {
-                    // if the first part is a number, just parse it
-                    value1 = part1.parse::<usize>().unwrap_or(0);
-                }
-    
-                // check if the second part is a label and exists in `labels`
-                if let Some(index2) = self.labels.iter().position(|label| label == part2) {
-                    if let Some(locctr_value2) = self.locctr.get(&index2) {
-                        if let Ok(locctr_numeric2) = usize::from_str_radix(locctr_value2, 16) {
-                            value2 = locctr_numeric2;
-                        }
-                    }
-                } else {
-                    // if the second part is a number, just parse it
-                    value2 = part2.parse::<usize>().unwrap_or(0);
-                }
-    
-                // subtract the two values
-                return value1.saturating_sub(value2);
-            }
-        } else if input.contains('+') {
-            let parts: Vec<&str> = input.split('+').collect();
-            if parts.len() == 2 {
-                let (part1, part2) = (parts[0].trim(), parts[1].trim());
-    
-                let mut value1 = 0;
-                let mut value2 = 0;
-    
-                if let Some(index1) = self.labels.iter().position(|label| label == part1) {
-                    if let Some(locctr_value1) = self.locctr.get(&index1) {
-                        if let Ok(locctr_numeric1) = usize::from_str_radix(locctr_value1, 16) {
-                            value1 = locctr_numeric1;
-                        }
-                    }
-                } else {
-                    value1 = part1.parse::<usize>().unwrap_or(0);
-                }
-
-                if let Some(index2) = self.labels.iter().position(|label| label == part2) {
-                    if let Some(locctr_value2) = self.locctr.get(&index2) {
-                        if let Ok(locctr_numeric2) = usize::from_str_radix(locctr_value2, 16) {
-                            value2 = locctr_numeric2;
-                        }
-                    }
-                } else {
-                    value2 = part2.parse::<usize>().unwrap_or(0);
-                }
-                return value1.saturating_add(value2);
-            }
-        }
-    
-        // return 0 if no valid operation was performed
-        0
-    }
-    
-    fn update_locctr(&mut self, i: usize, pass1_file: &mut File, symbol_table: &mut File, literals_table: &mut File) {
-        let instr_type = &self.instr[i];
-        let ref_type = &self.ref_data[i];
-        let labels_type = &self.labels[i];
-
-        let format1 = ["FIX", "FLOAT", "HIO", "SIO", "TIO", "NORM"];
-        let format2=["ADDR", "CLEAR", "COMPR", "DIVR", "MULR", "RMO", "SHIFTR", "SHIFTL", "SUBR", "SVC", "TIXR"];
-        /*let format3 = ["ADD", "ADDF", "AND", "COMP", "COMPF", "DIV", "J", "JEQ", "JGT", "JLT", "JSUB", "LDA", "LDB", "LDCH", "LDF", "LDL", "LDS", "LDT", "LDX", "LPS", "MUL", "MULF", "OR", "RD", "RSUB", "SSK", "STA", "STB", "STCH", "STF", "STI", "STL", "STS", "STSW", "STT", "STX", "SUB", "SUBF", "TD", "TIX", "WD"];*/
-        let format4f=["CADD", "CSUB", "CLOAD", "CSTORE", "CJUMP"];
-        let defaultblock = ["DEFAULT"];
-        let defaultb = ["DEFAULTB"];
-        let cdata = ["CDATA"];
-        let cblks = ["CBLKS"];
-
-        if i == 0 {
-            self.locctr.write_to_file(pass1_file, i, instr_type, ref_type, labels_type).unwrap();
-        }
-
-        else {
-            let base_locctr_str: Option<&String> = self.locctr.get(&i);
-            let base_locctr: usize = match base_locctr_str {Some(val) => usize::from_str_radix(&val, 16).unwrap_or(0), None => 0,};
-
-            let default_locctr_str: Option<&String> = self.default_locctr.get(&i);
-            let default_locctr: usize = match default_locctr_str {Some(val) => usize::from_str_radix(&val, 16).unwrap_or(0), None => 0,};
-
-            let defaultb_locctr_str: Option<&String> = self.defaultb_locctr.get(&i);
-            let defaultb_locctr: usize = match defaultb_locctr_str {Some(val) => usize::from_str_radix(&val, 16).unwrap_or(0), None => 0,};
-
-            let cdata_locctr_str: Option<&String> = self.cdata_locctr.get(&i);
-            let cdata_locctr: usize = match cdata_locctr_str {Some(val) => usize::from_str_radix(&val, 16).unwrap_or(0), None => 0,};
-
-            let cblks_locctr_str: Option<&String> = self.cblks_locctr.get(&i);
-            let cblks_locctr: usize = match cblks_locctr_str {Some(val) => usize::from_str_radix(&val, 16).unwrap_or(0), None => 0,};
-
-            let (new_locctr, new_default_locctr, new_defaultb_locctr, new_cdata_locctr, new_cblks_locctr, default_boolean, defaultb_boolean, cdata_boolean, cblks_boolean) = match instr_type.as_str() {
-                _ if format1.contains(&instr_type.as_str()) => (base_locctr + 1, default_locctr + 1, defaultb_locctr, cdata_locctr, cblks_locctr, true, false, false, false),
-                _ if format2.contains(&instr_type.as_str()) => (base_locctr + 2, default_locctr + 2, defaultb_locctr, cdata_locctr, cblks_locctr, true, false, false, false),
-                _ if format4f.contains(&instr_type.as_str()) | instr_type.starts_with('+') => (base_locctr + 4, default_locctr, defaultb_locctr + 4, cdata_locctr, cblks_locctr, false, true, false, false),
-                "WORD" => (base_locctr + 3, default_locctr, defaultb_locctr, cdata_locctr + 3, cblks_locctr, false, false, true, false),
-                "BYTE" => (base_locctr + self.calculate_byte_size(ref_type), default_locctr, defaultb_locctr, cdata_locctr + self.calculate_byte_size(ref_type), cblks_locctr, false, false, true, false),
-                "RESW" => {
-                    (base_locctr + ref_type.parse::<usize>().unwrap_or(0) * 3, default_locctr, defaultb_locctr, cdata_locctr, cblks_locctr + ref_type.parse::<usize>().unwrap_or(0) * 3, false, false, false, true)
-                },
-                "RESB" => {
-                    (base_locctr + ref_type.parse::<usize>().unwrap_or(0) * 1, default_locctr, defaultb_locctr, cdata_locctr, cblks_locctr + ref_type.parse::<usize>().unwrap_or(0) * 1, false, false, false, true)
-                },
-                //handling literals 
-                _ if instr_type.starts_with("=") => (base_locctr + self.calculate_literal_byte_size(instr_type), default_locctr, defaultb_locctr, cdata_locctr + self.calculate_literal_byte_size(instr_type), cblks_locctr, false, false, true, false),
-                "EQU" => {
-                    if ref_type.starts_with('*') {
-                        (base_locctr, default_locctr, defaultb_locctr, cdata_locctr, cblks_locctr, false, false, false, true)
-                    } else {
-                        let diff = self.calculate_difference(ref_type);
-                        (diff, default_locctr, defaultb_locctr, cdata_locctr, diff, false, false, false, true)
-                    }
-                },
-                "BASE" | "LTORG" | "END" => (string_to_usize(""), string_to_usize(""), string_to_usize(""), string_to_usize(""), string_to_usize(""), false, true, false, false),
-                _ if defaultblock.contains(&ref_type.as_str()) => (base_locctr, default_locctr, defaultb_locctr, cdata_locctr, cblks_locctr, true, false, false, false),
-                _  if defaultb.contains(&ref_type.as_str()) => (base_locctr, default_locctr, defaultb_locctr, cdata_locctr, cblks_locctr, false, true, false, false),
-                _  if cdata.contains(&ref_type.as_str()) => (base_locctr, default_locctr, defaultb_locctr, cdata_locctr, cblks_locctr, false, false, true, false),
-                _ if cblks.contains(&ref_type.as_str()) => (base_locctr, default_locctr, defaultb_locctr, cdata_locctr, cblks_locctr, false, false, false, true),
-                _ => (base_locctr + 3, default_locctr, defaultb_locctr + 3, cdata_locctr, cblks_locctr, false, true, false, false), //default to format 3
-            };
-
-            self.locctr.turn_to_hexa(i, new_locctr, instr_type, ref_type);
-            self.default_locctr.turn_to_hexa(i, new_default_locctr, instr_type, ref_type);
-            self.defaultb_locctr.turn_to_hexa(i, new_defaultb_locctr, instr_type, ref_type);
-            self.cdata_locctr.turn_to_hexa(i, new_cdata_locctr, instr_type, ref_type);
-            self.cblks_locctr.turn_to_hexa(i, new_cblks_locctr, instr_type, ref_type);
-
-            // checks if sic/xe
-            let mut sic: bool = true;
-            for i in 0..self.instr.len() {
-                if format1.contains(&self.instr[i].as_str()) || format2.contains(&self.instr[i].as_str()) || format4f.contains(&self.instr[i].as_str()) || self.instr[i].starts_with("+") {
-                    sic = false;
-                    break;
-                } 
-            }
-
-            if sic == false {
-                // logic to check block used then write to file
-                if default_boolean == true{
-                    self.default_locctr.write_to_file(pass1_file, i, instr_type, ref_type, labels_type).unwrap();
-                }
-                else if defaultb_boolean == true {
-                    self.defaultb_locctr.write_to_file(pass1_file, i, instr_type, ref_type, labels_type).unwrap();
-                }
-                else if cdata_boolean == true {
-                    self.cdata_locctr.write_to_file(pass1_file, i, instr_type, ref_type, labels_type).unwrap();
-                }
-                else if cblks_boolean == true {
-                    self.cblks_locctr.write_to_file(pass1_file, i, instr_type, ref_type, labels_type).unwrap();
-
-                } //else cblks
-            }
-            else {
-                self.locctr.write_to_file(pass1_file, i, instr_type, ref_type, labels_type).unwrap();
-            }
-
-            //generate symbol table and literal table
-            self.locctr.generate_symbol_table(symbol_table, labels_type, i).unwrap();
-            if let Some(literals_type) = self.literals.get(&i) {
-                self.locctr.generate_literal_table(literals_table, literals_type, i).unwrap();
-            }            
-            
-        }
-
-    }
-
     fn calculate_byte_size(&self, ref_type: &str) -> usize {
         if ref_type.starts_with('X') {
             (ref_type.len() - 3) / 2
@@ -474,17 +253,4 @@ impl Pass1 {
             0
         }
     }
-
-    fn calculate_literal_byte_size(&self, instr_type: &str) -> usize {
-        if instr_type.contains('X') {
-            (instr_type.len() - 4) / 2
-        } 
-        else if instr_type.contains('C') {
-            instr_type.len() - 4
-        } 
-        else {
-            0
-        }
-    }
-
 }
